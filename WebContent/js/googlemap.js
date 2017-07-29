7c5f1db3fd8ad410983d773b8eda5c185a0d8b45
seiyatakahashi <swkt1410@gmail.com>
2017-07-29 14:11:59 +0900

    文字列を編集

--- WebContent/js/googlemap.js
+++ WebContent/js/googlemap.js
@@ -4,13 +4,13 @@ var infoWindow;
 
 
 var center = {
-   lat: 35.626748, // 緯度
-  lng: 139.633986 // 経度
+   lat: 34.462151, // 緯度
+  lng: 135.736970 // 経度
 };
 function initMap() {
  map = new google.maps.Map(document.getElementById('map'), { // #mapに地図を埋め込む
      center: center, // 地図の中心を指定
-     zoom: 16 // 地図のズームを指定
+     zoom: 9 // 地図のズームを指定
    });
 
  marker = new google.maps.Marker({ // マーカーの追加
@@ -19,7 +19,7 @@ function initMap() {
    });
 
  infoWindow = new google.maps.InfoWindow({ // 吹き出しの追加
-        content: '<div class="map">ここです！！！</div>' // 吹き出しに表示する内容
+        content: '<div class="map">ここでーす！！！</div>' // 吹き出しに表示する内容
   });
  marker.addListener('click', function() { // マーカーをクリックしたとき
      infoWindow.open(map, marker); // 吹き出しの表示
--- WebContent/js/test.js
+++ WebContent/js/test.js
@@ -31,6 +31,80 @@ Login.doError = function doError(msg) {
 	alert(msg);
 	return false;
 }
+
+//クッキーにて訪問数をカウント
+window.onload = function () {
+
+    // クッキーに保存した訪問回数を取得し、変数「$count」に格納
+    var $count = getCookie( 'sampleNumberOfVisits' );
+
+    if( !$count ){ // 変数「$count」が空の場合の処理
+        setCookie( 'sampleNumberOfVisits', 1, 1 );
+    }else{ // 変数「$count」が空でない場合の処理
+        setCookie( 'sampleNumberOfVisits', ++$count, 1 );
+    }
+
+    // 訪問回数を表示する関数を呼び出す
+    displayCookie( 'sampleNumberOfVisits' );
+}
+
+// クッキーを設定する関数
+function setCookie( $cookieName, $cookieValue, $days ){
+
+    // Dateオブジェクトを生成
+    var $dateObject = new Date();
+
+    // クッキーの有効期限を示すDateオブジェクトを生成
+    $dateObject.setTime( $dateObject.getTime() + ( $days*24*60*60*1000 ) );
+
+    // 有効期限をグリニッジ標準時に変換
+    // 有効期限をクッキーに設定するための文字列を、変数「$expires」に代入
+    var $expires = "expires=" + $dateObject.toGMTString();
+
+    // 有効期限付きでクッキーを設定
+    document.cookie = $cookieName + "=" + $cookieValue + "; " + $expires;
+}
+
+// クッキーを取得する関数
+function getCookie( $cookieName ){
+
+    // 「;」区切りで分割し、配列「$cookies」に格納
+    var $cookies = document.cookie.split(';');
+
+    // クッキーの数だけループ
+    for( var $i=0; $i < $cookies.length; $i++ ){
+
+        // クッキーを「=」区切りで分割し、配列「$cookie」に格納
+        var $cookie = $cookies[$i].trim().split( '=' );
+
+        // クッキー名が仮引数「$cookieName」と同じであれば値を返す
+        if( $cookie[0] == $cookieName ){
+            return $cookie[1];
+        }
+    }
+    return "";
+}
+
+// クッキーを削除する関数
+function deleteCookie( $cookieName ){
+
+    // 過ぎた有効期限を設定することで削除できる
+    document.cookie = $cookieName + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+
+    // 訪問回数を表示する関数を呼び出す
+    displayCookie( $cookieName );
+}
+
+// 訪問回数を表示する関数
+function displayCookie( $cookieName ){
+
+    // クッキーに保存した訪問回数を取得
+    var $cookieValue = getCookie( $cookieName );
+
+    // 訪問回数を表示
+    document.getElementById( 'sampleOutput' ).innerHTML = $cookieValue;
+}
+
 //okvideo部分
 $(function(){
     $(".video").okvideo({ source: 'https://vimeo.com/129901967',
